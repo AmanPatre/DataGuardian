b22f94a2403733ca3a86f7ce4e96c21e6a246f41
AmanPatre <amanpatre25@gmail.com>
2025-09-23 16:41:08 +0530

    Added backend logs

--- backend/routes/siteRoutes.js
+++ backend/routes/siteRoutes.js
@@ -1,5 +1,23 @@
 import { Router } from "express";
-import { analyzeSite, getSite, getAllSites , getNetworkGraph } from "../controllers/siteController.js";
+import {
+  analyzeSite,
+  getSite,
+  getAllSites,
+  getNetworkGraph,
+} from "../controllers/siteController.js";
+import swaggerJsdoc from "swagger-jsdoc";
+
+/**
+ * @swagger
+ * /api/sites/analyze:
+ *   post:
+ *     summary: Analyze a website for trackers
+ *     parameters:
+ *       - name: url
+ *         required: true
+ *         schema:
+ *           type: string
+ */
 
 const siteRoutes = Router();
 
@@ -7,7 +25,5 @@ siteRoutes.post("/analyze", analyzeSite);
 siteRoutes.get("/network", getNetworkGraph);
 siteRoutes.get("/:url", getSite);
 siteRoutes.get("/", getAllSites);
-
-
 
 export default siteRoutes;
